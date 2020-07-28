2a14da77549a609a1789d2109c7ba94e24d17f7e
AMuttock1 <am6819@ic.ac.uk>
2020-07-28 03:15:21 +0100

    Update on Solver
    Iterative solver updated. Solves top down. Doesnt really create any useful results, but maybe will work in tandem with solver down up...

--- objtTree.py
+++ objtTree.py
@@ -8,8 +8,8 @@ Created on Thu Jul  9 16:29:57 2020
 #using object oriented approach to creating a branching tree in 2D
 
 
-import numpy
-
+import numpy as np
+from scipy.optimize import fsolve
 #import svgwrite
 #def writeVtkStructuredGrid(StructuredGrid, file_name):
 #    """
@@ -30,11 +30,11 @@ initVelocityList=[];
 #some global terms:
 
 #number of generations
-finalGen = 10;
+finalGen = 7;
 #volume flow rate in trachea
 Q0 = 0.0005;
 #area of trachea
-A0 = numpy.pi*((0.018/2)**2);
+A0 = np.pi*((0.018/2)**2);
 #v1
 v1 = Q0 / A0;
 #length of trachea, check these numbers 
@@ -48,20 +48,20 @@ rho = 1.225;
 #viscosty of air (assumed constant)
 mew = 0.00001857;
 #P1 is found from a small pressure loss in trachea 
-P1 = P0 - ((8*numpy.pi*mew*L1*v1)/A0);
+#P1 = P0 - ((8*numpy.pi*mew*L1*v1)/A0);
 
 
 
    
 
 class branch:
-    def __init__(self, length, dia, gen):
+    def __init__(self, length, dia, gen, myIndex):
         self.length = length;
         self.dia = dia;
        # self.vel = vel;
         self.gen = gen;
        # self.number = number;
-        self.area = (numpy.pi*(self.dia)**2)/4;
+        self.area = (np.pi*(self.dia)**2)/4;
         self.fin = False;
        
         #create array of velocities and pressures assigend to bronci and associated node
@@ -69,22 +69,25 @@ class branch:
         
         self.velEstimate = [];
         self.presEstimate = [];
-        self.presEstimate.append(100000);
+        #self.presEstimate.append(100000);
         
         
          #for now losses are derived by poiseuille flow only (laminar flow friction in pipes)
-        self.pLoss = [];
+        #self.pLoss = [];
         
         #initialise index of daughter branches so that the daughter variables can 
         #be retrieved 
+        self.myIndex = myIndex;
+        
         self.daughterIndexA = int;
         self.daughterIndexB = int;
-        
+        self.siblingIndex = int;
         
         self.daughterVelA = [];
         self.daughterVelB = [];
         self.daughterPresA = [];
         self.daughterPresB = [];
+        
         
   
         
@@ -98,30 +101,59 @@ class branch:
         genD = self.gen + 1;
        # velD = self.vel*self.area/(2*areaD);
         self.fin = True;
-        
+       # self.myIndex = len(branches)-1;
         #construct the daughter branches 
-        daughterA = branch(lenD, diaD, genD);
-        daughterB = branch(lenD, diaD, genD);
-        branches.append(daughterA);
+        #daughter branches must have and index with respect to parent 
+        #daughter branches must have the index of their sibling
+        daughterA = branch(lenD, diaD, genD, len(branches));
         self.daughterIndexA = len(branches);
-        branches.append(daughterB);
+        branches.append(daughterA);
+        
+        daughterB = branch(lenD, diaD, genD, len(branches));
         self.daughterIndexB = len(branches);
+        branches.append(daughterB);
+       # print("My index:", self.myIndex, "D. A index:", self.daughterIndexA,"D. B index:", self.daughterIndexB,"My gen:", self.gen)  
+       # branches[self.daughterIndexA].siblingIndex = self.daughterIndexB;
+        #branches[self.daughterIndexB].siblingIndex = self.daughterIndexA;
+        
         
         
     #function to calculate the poiseulle losses in the pipe
     #velocity estimate taken from symetric, frictionless instance
     def Poiseuille(self):
       
-        poise =  ((8*numpy.pi*mew*self.length*self.velEstimate[-1])/self.area);
-        self.pLoss.append(poise);
-    
+        pLoss =  ((8*np.pi*mew*self.length*self.velEstimate[-1])/self.area);
+     
+        #this function has been copied into where its needed later on
+        
+        return pLoss
     #function to assign initialised velocity
     
     def initVel(self):
-        self.velEstimate.append(initVelocityList[(self.gen)-1]);
+        self.velEstimate.append(initVelocityList[(self.gen)]);
      
+    #funciton to assign initialised pressures
     
-    
+    def initPres(self):
+        
+        if self.gen == 0:
+            
+            pLoss = self.Poiseuille();
+            Pres1 = P0 - pLoss;
+            #print(P0, Pres1, P1)
+            self.presEstimate.append(Pres1);
+            
+        else:
+            
+            #first guess is atmospheric 
+            
+            self.presEstimate.append(100000)
+            
+            pass
+        
+        
+        
+        
     # def branchDataInit(self):
     #     #first we must must initialise the estimated velocities in all nodes
         
@@ -136,10 +168,48 @@ class branch:
     #     pass
     
     
-    def functions(z):
+    def generalFunctionA(self, z):
+    
+        #will feeding down work?????
+        #I have a feeling it wont
         
-        vel=
+        VSolveA = z[0];
+        VSolveB = z[1];    
+        PSolveA = z[2];
         
+        f = np.zeros(3);
+        
+        f[0] = self.area*self.velEstimate[-1] - (VSolveA*branches[self.daughterIndexA].area) - (VSolveB*branches[self.daughterIndexB].area);
+        
+        f[1] = self.presEstimate[-1]  + (0.5*rho*self.velEstimate[-1]**2)  - PSolveA - (0.5*rho*VSolveA**2)    - ((8*np.pi*mew*branches[self.daughterIndexA].length*VSolveA)/branches[self.daughterIndexA].area)
+        
+        f[2] = self.presEstimate[-1]   + (0.5*rho*self.velEstimate[-1]**2)  - branches[self.daughterIndexB].presEstimate[-1]  - (0.5*rho*VSolveB**2)  - ((8*np.pi*mew*branches[self.daughterIndexB].length*VSolveB)/branches[self.daughterIndexB].area)
+        
+        return f
+    
+    
+    def generalFunctionB(self, y):
+        
+        #solve the pressure of the other daughter
+        #two options here. Carry forward P1 because it is more known, or 
+        #use PA estimate for PB to make it symetric. Not sure... 
+        #(going to start with P1)
+        #I can't mirror the above equations as it may result in a different 
+        #velocity and that would be a disaster
+        
+        PSolveB = y;
+        
+        f = 0;
+        
+        #### this format will require this function to immidiately access 
+        #VSolveB as calculated above
+        
+        f = (self.presEstimate[-1]+(0.5*rho*self.velEstimate[-1]**2)) - (PSolveB + (0.5*rho*branches[self.daughterIndexB].velEstimate[-1]**2) + ((8*np.pi*mew*branches[self.daughterIndexB].length*branches[self.daughterIndexB].velEstimate[-1])/branches[self.daughterIndexB].area))
+        
+        return f
+    
+    
+    
     
     #function to iteratively solve the pressure and velocity at every node.
     #this node solve function is applicable to the branches with daughters, 
@@ -159,31 +229,75 @@ class branch:
         PresA = self.daughterPresA[-1];
         PresB = self.daughterPresB[-1];  
                        
-        newVel = float;
-        newPress = float;
+        newVelA = float;
+        newPresA = float;
+        newVelB = float;
+        newPresB = float;
         
+        # Guess values for genFuncA appear as follows in the function:
+        #VSolveA = z[0];
+        #VSolveB = z[1];    
+        #PSolveA = z[2];
         
-        
-        
+        funcAGuess = [VelA,VelB,PresA];
+        funcBGuess = PresB;
+        if self.gen == finalGen:
+            
+            #call the final function
+            pass
+        else:
+            #call the general function A then general function B for each daughter
+            
+            z = fsolve(self.generalFunctionA,funcAGuess)
+            
+            newVelA = z[0];
+            newVelB = z[1];
+            newPresA = z[2];
+            
+            branches[self.daughterIndexA].velEstimate.append(newVelA);
+            branches[self.daughterIndexB].velEstimate.append(newVelB);
+            branches[self.daughterIndexA].presEstimate.append(newPresA);
+            
+            #now that the new calculations have been assigned, the calculation for B can begin
+            
+            y = fsolve(self.generalFunctionB, funcBGuess)
+            
+            newPresB = y;
+            
+            branches[self.daughterIndexB].presEstimate.append(newPresB);
+            
+            #print (z,y)
+            
+            pass
         
         
         
         pass
         
   
-
+##### end of class
+        
+    
+    
     #function to call the nodeSolve and check the convergence of results
 def callNodeSolve():
     
-    converged = False;
+   # converged = False;
     
-    
-    for i in range(len(branches)):
-        branches[i].nodeSolve();
-        
+   # while converged == False:
+    for j in range(5):
+        print(j)
+        for i in range(len(branches)):
+            if branches[i].gen <  finalGen - 1 :
+                branches[i].nodeSolve();
+              #  if (len(branches[i].velEstimate) >= 3):
+              #      print("Branch", branches[i].myIndex, "has", len(branches[i].velEstimate),  "entries")
+              #      if ( branches[i].velEstimate[-1] - branches[i].velEstimate[-2] ) < 0.0001:
+                        #converged = True;
+                       # print ("converged")
 
-    for in range(len(branches)):
-        if 
+   # for in range(len(branches)):
+    #    pass 
             
     
 
@@ -203,10 +317,10 @@ def frictionless():
             pass
         
     #velocity in final gen vZ given by below equation
-    vZ= Q0/totalAreaFinal;
+##    vZ= Q0/totalAreaFinal;
     #pressure in final gen PZ given by below equation
-    PZ = P1 +(0.5*rho*((v1**2)-(vZ**2)));
-    print( 'PZ =', PZ,'vZ=', vZ,'totalAreaFinal =', totalAreaFinal)
+    #PZ = P1 +(0.5*rho*((v1**2)-(vZ**2)));
+##    print('vZ=', vZ,'totalAreaFinal =', totalAreaFinal)
     #a peculiar result arises. PZ must be higher that P1 according to bernoulli's law,
     #because the area total at end is greater, thus the velocity at the end must 
     #be lower, thus the dynamic pressure decreases, thus static pressure must 
@@ -218,14 +332,18 @@ def frictionless():
     #create a list of all the total areas of the different generations 
     areaList=[];
     
-    for i in range(finalGen):
+    #offset generations by 1, because the first generation (the trachea) is 
+    #considered to be generation zero
+    
+    for i in range(finalGen+1):
         
          areaList.append(0)
   # print (areaList)
     
     for i in range(len(branches)):
-        for j in range(finalGen):
-            if branches[i].gen == j:
+        for j in range(finalGen+1):
+           # print (j)
+            if branches[i].gen == (j):
                 areaList[j] += branches[i].area
             else:
                 pass
@@ -242,19 +360,24 @@ def frictionless():
     for i in range(len(branches)):
         branches[i].initVel()
         
+        
     for i in range(len(branches)):
-        branches[i].Poiseuille()
+        branches[i].initPres()
+    #for i in range(len(branches)):
+    #    branches[i].Poiseuille()
     
     #print (initVelocityList)
     
 
      
 def constructTree():
-    trach =  branch(0.12, 0.018, 0);
+    trach =  branch(0.12, 0.018, 0, 0);
     branches.append(trach);
     try:
         for i in range(2**(finalGen+1)):
-            while branches[i].gen <finalGen:
+            
+            #not sure if this is < or <=
+            while branches[i].gen < finalGen:
                 if branches[i].fin == False:
                     
                     branches[i].split()
@@ -266,8 +389,10 @@ def constructTree():
         
      
 constructTree();
-frictionless()  
+frictionless();
+callNodeSolve();  
+
 print (branches[2].length)
 print(len(branches))
-print(branches[-1].pLoss)
+#print(branches[-1].pLoss)
 
